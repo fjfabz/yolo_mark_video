05b3101c3afdb882f016bc8ade35a6b416cc3a99
Takayuki Mizutani <mizutani.takayuki@gmail.com>
2017-12-15 17:13:40 +0900

    add files

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # yolo_mark_video
-marking from video for darknet yolo
+To mark from video for darknet yolo
+
+# Usage
+
+python yolo_mark_video.py [video.mp4]
+python yolo_mark_video.py [video_folder]
+
+
+Move mouse and hit space. So marked txt and captured image are written in "img" folder.
+Marked size can change to hit button 1,2,3,4,5,6.
+
+1:bigger
+2:smaller
+3:bigger width
+4:smaller width
+5:bigger height
+6:smaller height
+
+
+You can confirm the marking by using Yolo_mark.
+https://github.com/AlexeyAB/Yolo_mark
---
+++ yolo_mark_video.py
@@ -0,0 +1,126 @@
+﻿import cv2
+import time
+import sys,os
+import glob
+import numpy as np
+
+img_counter = 0
+mousex = 0
+mousey = 0
+bound_width = 100
+bound_height = 100
+
+frame = None
+outfolder = None
+img_width = 0
+img_height = 0
+name = None
+
+def write_txt_img():
+    global img_counter
+    if not os.path.exists(outfolder): os.mkdir(outfolder)
+    # write txt
+    bounding_str = "0 {} {} {} {}".format(mousex / img_width, mousey / img_height, bound_width / img_width,
+                                          bound_height / img_height)
+    txt_name = "{0}_{1:03d}.txt".format(name, img_counter)
+    f = open(os.path.join(outfolder, txt_name), 'w')
+    f.write(bounding_str)
+    f.close()
+    # write img
+    img_name = "{0}_{1:03d}.jpg".format(name, img_counter)
+    cv2.imwrite(os.path.join(outfolder, img_name), frame)
+    print(img_name)
+    img_counter += 1
+
+def click_and_mark(event, x, y, flags, param):
+    global mousex
+    global mousey
+    mousex=x
+    mousey=y
+    clone = frame.copy()
+    cv2.putText(clone, '[space]:mark, [s]:stop, [1]:big, [2]:small, [esc]:next', (10, img_height-20), cv2.FONT_HERSHEY_PLAIN, 1.2,
+                (255, 255, 255), 1, cv2.LINE_AA)
+    if event == cv2.EVENT_MOUSEMOVE:
+        cv2.rectangle(clone, (x - round(bound_width/2), y - round(bound_height/2)), (x + round(bound_width/2), y + round(bound_height/2)), (0, 255, 0), 2)
+        cv2.imshow("image", clone)
+    elif event == cv2.EVENT_LBUTTONDOWN:
+        pass
+        #write_txt_img()
+
+
+def mark_process(file_path):
+    global frame,clone,outfolder,img_height,img_width,name,bound_height,bound_width
+
+    folder, basename = os.path.split(file_path)
+    name, ext = os.path.splitext(basename)
+    outfolder = os.path.join(folder, "img")
+
+    cam = cv2.VideoCapture(file_path)
+    fps = cam.get(cv2.CAP_PROP_FPS)
+
+    cv2.namedWindow("image")
+    cv2.setMouseCallback("image", click_and_mark)
+
+    _, frame = cam.read()
+    img_height, img_width = frame.shape[:2]
+    STOP = False
+
+    while True:
+        if not STOP:
+            ret, frame = cam.read()
+
+            if not ret:
+                break
+            clone=frame.copy()
+            cv2.putText(clone, '[space]:mark, [s]:stop, [1]:big, [2]:small, [esc]:next', (10, img_height-20), cv2.FONT_HERSHEY_PLAIN, 1.2,
+                            (255, 255, 255), 1, cv2.LINE_AA)
+            cv2.imshow("image", clone)
+            time.sleep(1 / fps)
+
+        k = cv2.waitKey(1)
+
+        if k % 256 == 27:
+            # ESC pressed
+            print("Escape hit, closing...")
+            break
+        elif k % 256 == 32:  # SPACE pressed. save image and crop area
+            write_txt_img()
+        elif k % 256 == 49:  # 1 pressed
+            bound_width *= 1.5
+            bound_height *= 1.5
+        elif k % 256 == 50:  # 2 pressed
+            bound_width /= 1.5
+            bound_height /= 1.5
+        elif k % 256 == 51:  # 3 pressed
+            bound_width *= 1.5
+            #bound_height *= 1.5
+        elif k % 256 == 52:  # 4 pressed
+            bound_width /= 1.5
+            #bound_height /= 1.5
+        elif k % 256 == 53:  # 5 pressed
+            #bound_width *= 1.5
+            bound_height *= 1.5
+        elif k % 256 == 54:  # 6 pressed
+            #bound_width /= 1.5
+            bound_height /= 1.5
+        elif k % 256 == 115:  # s pressed STOP and START
+            STOP = not STOP
+        elif k != -1:
+            print(k % 256)
+    cam.release()
+    cv2.destroyAllWindows()
+
+def main():
+
+    if os.path.isdir(sys.argv[1]):
+        filelists = glob.glob(sys.argv[1]+"/*.mp4")
+        for file_path in filelists :
+            print(file_path)
+            mark_process(file_path)
+    else:
+        file_path = "mongoose1.mp4"
+        file_path = sys.argv[1]
+        mark_process(file_path)
+
+if __name__ == '__main__':
+    main()
